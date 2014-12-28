705fee5ede2918db3c2f1fe0263fa8a9705ed4b4
Micah Elizabeth Scott <micah@scanlime.org>
2014-12-28 12:07:09 -0800

    Faster dot placement, nicer colors

--- fabricator/panel_to_pdf.js
+++ fabricator/panel_to_pdf.js
@@ -4,8 +4,8 @@ var paper = require('paper'),
     path = require('path'),
     fs = require('fs');
 
-var inFile = path.resolve(__dirname, '../layout/growth.json')
-var outFile = path.resolve(__dirname, 'panel.pdf')
+var inFile = path.resolve(__dirname, '../layout/growth.json');
+var outFile = path.resolve(__dirname, 'panels.pdf');
 
 var canvas = new paper.Canvas(612, 792, 'pdf');
 var fab = require('./fabricator.pjs')(canvas);
@@ -15,13 +15,16 @@ with (fab) {
         if (err) throw err;
         var growth = new Growth(JSON.parse(data));
         var scale = new Scale();
-
-        var panel = new Panel(growth, scale, 0);
-        var layer = panel.draw();
-
         var world = new Group();
-        world.addChild(scale.drawGrid(layer.bounds));
-        world.addChild(layer);
+
+        for (var panelNumber = 0; panelNumber < growth.json.panels.length; panelNumber++) {
+            console.log('Panel ' + panelNumber);
+
+            var panel = new Panel(growth, scale, panelNumber);
+            var layer = panel.draw();
+            world.addChild(scale.drawGrid(layer.bounds));
+            world.addChild(layer);
+        }
 
         world.fitBounds(view.bounds.expand(-100));
         world.scale(1, -1);
