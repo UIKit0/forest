b9e2bdf3cdc8b27367c2188992b2cf541cb3e6f7
Micah Elizabeth Scott <micah@scanlime.org>
2014-12-28 00:05:46 -0800

    Get started with node and paperjs

---
+++ fabricator/growth_to_pdf.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var paper = require('paper'),
+    path = require('path'),
+    fs = require('fs');
+
+var inFile = path.resolve(__dirname, '../layout/growth.json')
+var outFile = path.resolve(__dirname, 'growth.pdf')
+
+var canvas = new paper.Canvas(612, 792, 'pdf');
+var fab = require('./fabricator.pjs')(canvas);
+
+with (fab) {
+    fs.readFile(inFile, { encoding: 'utf8' }, function (err, data) {
+        if (err) throw err;
+        var growth = JSON.parse(data);
+
+        drawGrowth(growth);
+
+        view.update();
+        fs.writeFile(outFile, canvas.toBuffer(), function (err) {
+            if (err)
+                throw err;
+            console.log('Saved!');
+        });
+    });
+}
