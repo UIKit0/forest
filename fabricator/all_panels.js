a301d7a967d8b64bf97358bc0fb3f14b92bac58d
Micah Elizabeth Scott <micah@scanlime.org>
2015-01-11 15:49:52 -0800

    Scripts to generate all panels

--- fabricator/all_panels.js
+++ fabricator/all_panels.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
 var paper = require('./lib/paper-node.js'), fs = require('fs');
-var canvas = new paper.Canvas(792, 612, 'pdf');
-var fab = require('./fabricator.pjs')(canvas);
 
-with (fab) {
+function newDrawing(inchWidth, inchHeight) {
+    var canvas = new paper.Canvas(inchWidth * 72, inchHeight * 72, 'pdf');
+    return require('./fabricator.pjs')(canvas);
+}
+
+with (newDrawing(8.5, 11)) {
     var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
     var scale = new Scale();
     var panels = new Group();
@@ -18,5 +21,39 @@ with (fab) {
 
     MakeDiagramPage(panels, scale);
     view.update();
-    fs.writeFileSync('panels.pdf', canvas.toBuffer());
+    fs.writeFileSync('panels-diagram.pdf', view.element.toBuffer());
+}
+
+with (newDrawing(20*12, 12*12)) {
+    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+    var scale = new Scale();
+    var panels = new Group();
+
+    for (var panelNumber = 0; panelNumber < growth.json.panels.length; panelNumber++) {
+        console.log('Panel middle ' + panelNumber);
+
+        var panel = new Panel(growth, scale, panelNumber);
+        panels.addChild(panel.drawMiddle())
+    }
+
+    MakeActualSizePage(panels, scale);
+    view.update();
+    fs.writeFileSync('panels-middle.pdf', view.element.toBuffer());
+}
+
+with (newDrawing(20*12, 12*12)) {
+    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+    var scale = new Scale();
+    var panels = new Group();
+
+    for (var panelNumber = 0; panelNumber < growth.json.panels.length; panelNumber++) {
+        console.log('Panel front ' + panelNumber);
+
+        var panel = new Panel(growth, scale, panelNumber);
+        panels.addChild(panel.drawFront())
+    }
+
+    MakeActualSizePage(panels, scale);
+    view.update();
+    fs.writeFileSync('panels-front.pdf', view.element.toBuffer());
 }
--- fabricator/all_single_panels.js
+++ fabricator/all_single_panels.js
@@ -1,49 +1,52 @@
 #!/usr/bin/env node
 
 var paper = require('./lib/paper-node.js'), fs = require('fs');
-var panelNumber = parseInt(process.argv[2] || '0');
 
 function newDrawing(inchWidth, inchHeight) {
     var canvas = new paper.Canvas(inchWidth * 72, inchHeight * 72, 'pdf');
     return require('./fabricator.pjs')(canvas);
 }
 
-with (newDrawing(8.5, 11)) {
-    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
-    var scale = new Scale();
-    var panel = new Panel(growth, scale, panelNumber);
+for (var panelNumber = 0; panelNumber < 16; panelNumber++) {
+    console.log('Panel ' + panelNumber);
 
-    MakeDiagramPage(panel.draw(), scale);
-    view.update();
-    fs.writeFileSync('panel-' + panelNumber + '-drawing.pdf', view.element.toBuffer());
-}
+    with (newDrawing(8.5, 11)) {
+        var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+        var scale = new Scale();
+        var panel = new Panel(growth, scale, panelNumber);
 
-with (newDrawing(5*12, 5*12)) {
-    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
-    var scale = new Scale();
-    var panel = new Panel(growth, scale, panelNumber);
+        MakeDiagramPage(panel.draw(), scale);
+        view.update();
+        fs.writeFileSync('panel-' + panelNumber + '-drawing.pdf', view.element.toBuffer());
+    }
 
-    MakeActualSizePage(panel.drawFront(), scale);
-    view.update();
-    fs.writeFileSync('panel-' + panelNumber + '-front.pdf', view.element.toBuffer());
-}
+    with (newDrawing(5*12, 5*12)) {
+        var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+        var scale = new Scale();
+        var panel = new Panel(growth, scale, panelNumber);
 
-with (newDrawing(5*12, 5*12)) {
-    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
-    var scale = new Scale();
-    var panel = new Panel(growth, scale, panelNumber);
+        MakeActualSizePage(panel.drawFront(), scale);
+        view.update();
+        fs.writeFileSync('panel-' + panelNumber + '-front.pdf', view.element.toBuffer());
+    }
 
-    MakeActualSizePage(panel.drawMiddle(), scale);
-    view.update();
-    fs.writeFileSync('panel-' + panelNumber + '-middle.pdf', view.element.toBuffer());
-}
+    with (newDrawing(5*12, 5*12)) {
+        var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+        var scale = new Scale();
+        var panel = new Panel(growth, scale, panelNumber);
 
-with (newDrawing(5*12, 5*12)) {
-    var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
-    var scale = new Scale();
-    var panel = new Panel(growth, scale, panelNumber);
+        MakeActualSizePage(panel.drawMiddle(), scale);
+        view.update();
+        fs.writeFileSync('panel-' + panelNumber + '-middle.pdf', view.element.toBuffer());
+    }
 
-    MakeActualSizePage(panel.drawBack(), scale);
-    view.update();
-    fs.writeFileSync('panel-' + panelNumber + '-back.pdf', view.element.toBuffer());
+    with (newDrawing(5*12, 5*12)) {
+        var growth = new Growth(JSON.parse(fs.readFileSync('../layout/growth.json')));
+        var scale = new Scale();
+        var panel = new Panel(growth, scale, panelNumber);
+
+        MakeActualSizePage(panel.drawBack(), scale);
+        view.update();
+        fs.writeFileSync('panel-' + panelNumber + '-back.pdf', view.element.toBuffer());
+    }
 }
