eef7f7c762fdd826c1ca4fcb61612a181ef43af2
Micah Elizabeth Scott <micah@scanlime.org>
2015-02-17 19:56:39 -0800

    Moar radius

--- circle-engine/src/FadecandyGL.cpp
+++ circle-engine/src/FadecandyGL.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 void FadecandyGL::setup(ci::app::App &app)
 {
-    samplingRadius = 8.0;
+    samplingRadius = 16.0;
     mProg = gl::GlslProg( app.loadResource("fadecandy.glslv"), app.loadResource("fadecandy.glslf") );
 }
 
