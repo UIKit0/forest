984d3d2c447c81b931fd538aec4541f30b781ce7
Micah Elizabeth Scott <micah@scanlime.org>
2015-02-25 16:38:33 -0500

    Example for panel 0

--- firmware/color_to_midi/color_to_midi.ino
+++ firmware/color_to_midi/color_to_midi.ino
@@ -12,20 +12,27 @@
 
 #include "spinner.h"
 
-Spinner spin1(22, 23, 27);
+//             SCL  SDA  Spinner # (0-31)
+Spinner spin1( 0,   1,   0  );
+Spinner spin2( 2,   3,   8  );
+Spinner spin3( 4,   5,   16 );
 
 const int LED = 13;
 
 void setup()
 {
- 	spin1.setup();
+    spin1.setup();
+    spin2.setup();
+    spin3.setup();
 
  	pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
- 	spin1.loop();
+    spin1.loop();
+    spin2.loop();
+    spin3.loop();
 
 	// Read and ignore incoming MIDI messages
 	while (usbMIDI.read());
